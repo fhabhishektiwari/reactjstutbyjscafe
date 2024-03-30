52e144952636b808253a016ef21dfab633bc0d21
fhabhishektiwari <abhishektiwarifh@gmail.com>
2024-03-30 23:05:42 +0530

    added setup with webpack

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@
 // div.appendChild(p);
 
 // createElement("element name",{attributes},"contents")
+import React from "react";
+import ReactDOM from "react-dom";
 const child1 = React.createElement("p", {}, "This is child1");
 const child2 = React.createElement("p", {}, "This is child2");
 
-const div = React.createElement("div", { className: "text" }, [child1, child2]);
+// const div = React.createElement("div", { className: "text" }, [child1, child2]);
+const div = <div>Hello React js developer</div>;//jsx
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(div);
